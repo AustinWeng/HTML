cf3b74dc8f3f9c028e8e6fbeccc94d9dd45d452c
AustinWeng <mchiao.weng@gmail.com>
2021-07-04 00:31:48 +0800

    Add Lin Notification

--- hospital.py
+++ hospital.py
@@ -3,7 +3,10 @@ from bs4 import BeautifulSoup
 import time
 
 url = 'https://bih.hch.gov.tw/web/page/news22-%E5%AD%95%E5%A9%A6%E7%96%AB%E8%8B%97%E9%96%80%E8%A8%BA%E5%85%AC%E5%91%8A.html'
-
+headers = {
+    "Authorization": "Bearer " + "ekuo8mZjzr3z00Lfk5ZQQwVWQPi9rh2VIJBg9NUBjnm", 
+    "Content-Type" : "application/x-www-form-urlencoded"
+}
 html = requests.get(url)
 html.encoding = 'utf8'
 #print(html.text)
@@ -17,4 +20,6 @@ for w3scholl in w3schollsList:
 for index,li in enumerate(ulList):
     #print(index)
     if(index >= 5):
-        print(li.getText())
+        #print(li.getText())
+        payload = {'message': li.getText() }
+        r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
